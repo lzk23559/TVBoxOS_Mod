6d0fe4f19b3c779a987bce34e5761cb07d35a3ed
liutao3611 <79844442+liutao3611@users.noreply.github.com>
2022-09-19 20:35:12 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -80,7 +80,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitcode.net/t1/tan/-/raw/master/tt.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
