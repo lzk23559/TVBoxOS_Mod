2f08ad642df66fc073642fbd75b65a4f9e2057e5
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2022-09-26 02:05:23 +0800

    Update FileUtils.java

--- app/src/main/java/com/github/tvbox/osc/util/FileUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/FileUtils.java
@@ -2,12 +2,15 @@ package com.github.tvbox.osc.util;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class FileUtils {
 
