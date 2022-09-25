a649076ab8550a189faeb2e6c88b3933fc60d7e0
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2022-09-26 02:28:06 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -30,7 +30,7 @@ public class App extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         instance = this;
-        RemoteConfig.Init();
+        RemoteConfig.Init(this);
         initParams();
         // OKGo
         OkGoHelper.init();
