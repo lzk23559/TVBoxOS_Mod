285d75f3adc3697c9d4ec4822711cde7564c7739
钊赢天下 <ballad0324@163.com>
2023-02-09 03:03:31 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/cyl518/yl/raw/branch/master/ml.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://raw.iqiq.io/liu673cn/box/main/m.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
