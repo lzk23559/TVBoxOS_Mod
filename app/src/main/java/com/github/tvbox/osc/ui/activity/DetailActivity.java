c0cebc81a6db23255949159dbe342c24333eb6f7
yangqiang4125 <Yangqiang3504>
2023-10-17 17:44:59 +0800

    fix - 101079

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -948,7 +948,7 @@ public class DetailActivity extends BaseActivity {
                             if (mVideo.pic != null) {
                                 if (ApiConfig.isPic(mVideo.pic)&&mVideo.pic.contains("doubanio")) {
                                     spPic = mVideo.pic;
-                                    if(sourceKey.equals("push_agentqq")&&!spPic.contains(wdPic)){
+                                    if(sourceKey.equals("push_agentqq")&&!spPic.contains(wdPic)&&!wdPic.contains(spPic)){
                                         updateData("notip" + spId + " " + spPic);
                                     }
                                     wdPic = spPic;
