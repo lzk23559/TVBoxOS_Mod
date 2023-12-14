6cddcaee0df4b54f7e75020b3b80c2b9f1fdaaf7
chenlong622 <121268613+chenlong622@users.noreply.github.com>
2023-12-14 18:14:36 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://jihulab.com/chenlong622/1/-/raw/main/long.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/chenlong622/LIVE/raw/main/long.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
