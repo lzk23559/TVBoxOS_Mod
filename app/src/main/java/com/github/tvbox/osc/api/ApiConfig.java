9dd2fb7e39c0f06cb3282c85f326e0245e2cd3af
3695059 <156566827+3695059@users.noreply.github.com>
2024-01-14 15:03:27 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://files.cnblogs.com/files/johnjackson/tvbox20230815.json?t=1692098981&download=true");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
