e72df944ede07e586f8aa57e6aceed79175135a9
670637080 <110383438+670637080@users.noreply.github.com>
2024-07-19 13:34:01 +0800

    Update CheckboxSearchAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/CheckboxSearchAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/CheckboxSearchAdapter.java
@@ -78,10 +78,8 @@ public class CheckboxSearchAdapter extends ListAdapter<SourceBean, CheckboxSearc
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     mCheckedSources.put(sourceBean.getKey(), "1");
-                    Hawk.put(HawkConfig.SOURCES_FOR_SEARCH, mCheckedSources);
                 } else {
-                    mCheckedSources.remove(sourceBean.getKey());
-                    Hawk.put(HawkConfig.SOURCES_FOR_SEARCH, mCheckedSources);
+                    mCheckedSources.put(sourceBean.getKey(), "0");
                 }
                 notifyItemChanged(pos);
             }
