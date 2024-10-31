969cf9ca2905125719b3e2cf4ce44b3000a81e14
jiqimaojiqimao <lilijuan_1980@163.com>
2024-10-31 18:37:39 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -57,7 +57,7 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         if (mRenderersFactory == null) {
             mRenderersFactory = new DefaultRenderersFactory(mAppContext);
         }
-        mRenderersFactory.setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);       //XUAMENG扩展优先
+ //       mRenderersFactory.setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);       //XUAMENG扩展优先
 //		mRenderersFactory.setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON);
         if (mTrackSelector == null) {
             mTrackSelector = new DefaultTrackSelector(mAppContext);
