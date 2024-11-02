749d5791ac3fa42bec59569d1386fb934530ee76
jiqimaojiqimao <lilijuan_1980@163.com>
2024-11-02 20:50:20 +0800

    Add files via upload

---
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/DescDialog.java
@@ -0,0 +1,34 @@
+package com.github.tvbox.osc.ui.dialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import com.github.tvbox.osc.R;
+import org.greenrobot.eventbus.EventBus;
+import org.jetbrains.annotations.NotNull;
+
+public class DescDialog extends BaseDialog {    
+
+    public DescDialog(@NonNull @NotNull Context context) {
+        super(context);       
+        setContentView(R.layout.dialog_desc);
+    }
+    
+    public void setDescribe(String describe) {
+    	TextView tvDescribe = findViewById(R.id.describe);
+        tvDescribe.setText("        内容简介：" + describe);  //xuameng
+        tvDescribe.requestFocus();
+        tvDescribe.requestFocusFromTouch();
+    }
+
+    private void init(Context context) {
+        EventBus.getDefault().register(this);
+        setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                EventBus.getDefault().unregister(this);
+            }
+        });
+    }
+}
